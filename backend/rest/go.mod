618b3bcbd8e5abc3c656fd1b8cc9101aa27873c8
Scott <mooseburger1@msn.com>
2021-12-23 13:23:44 -0600

    added angular frontend package

--- backend/rest/go.mod
+++ backend/rest/go.mod
@@ -3,3 +3,5 @@ module rest
 go 1.14
 
 require golang.org/x/net v0.0.0-20211216030914-fe4d6282115f
+
+require github.com/gorilla/mux v1.8.0
--- backend/rest/server.go
+++ backend/rest/server.go
@@ -8,6 +8,8 @@ import (
 	"rest/handlers"
 	"time"
 
+	"github.com/gorilla/mux"
+
 	"golang.org/x/net/context"
 )
 
@@ -16,18 +18,25 @@ func main() {
 	logger := log.New(os.Stdout, "rest-server", log.LstdFlags)
 
 	/////// Initialize handlers here ///////
-	hh := handlers.NewHelloStruct(l)
+	hh := handlers.NewHelloStruct(logger)
 
 	//Serve Mux to replace the default ServeMux
-	sm := http.NewServeMux()
+	serveMux := mux.NewRouter()
 
-	/////// Define route handlers for ServeMux here ///////
-	sm.Handle("/", hh)
+	//Create filtered Routers to handle specific verbs
+	getRouter := serveMux.Methods(http.MethodGet).Subrouter()
+	getRouter.HandleFunc("/", hh.ServeHTTP)
+
+	putRouter := serveMux.Methods(http.MethodPut).Subrouter()
+	putRouter.HandleFunc("/", hh.ServeHTTP)
+
+	postRouter := serveMux.Methods(http.MethodPost).Subrouter()
+	postRouter.HandleFunc("/", hh.ServeHTTP)
 
 	// Configure the server {TODO: move these to an external configurable file/location}
 	server := &http.Server{
 		Addr:         ":9090",
-		Handler:      sm,
+		Handler:      serveMux,
 		IdleTimeout:  120 * time.Second,
 		ReadTimeout:  1 * time.Second,
 		WriteTimeout: 1 * time.Second,
@@ -37,7 +46,7 @@ func main() {
 	go func() {
 		err := server.ListenAndServe()
 		if err != nil {
-			l.Fatal(err)
+			logger.Fatal(err)
 		}
 	}()
 
@@ -48,7 +57,7 @@ func main() {
 	//Block while not receiving forecfull shutdown
 	sig := <-sigChan
 
-	l.Println("Received terminate, graceful shutdown", sig)
+	logger.Println("Received terminate, graceful shutdown", sig)
 
 	//Define context to provide server on how to shutdown all running processes
 	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
