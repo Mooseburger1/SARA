b5b25200201bb5fd007a854ebe5325d4e1798a97
Mooseburger1 <mooseburger1@msn.com>
2021-12-22 22:52:14 -0600

    updated comments in REST server.go

--- backend/rest/server.go
+++ backend/rest/server.go
@@ -12,14 +12,19 @@ import (
 )
 
 func main() {
+	//Main logger
+	logger := log.New(os.Stdout, "rest-server", log.LstdFlags)
 
-	l := log.New(os.Stdout, "rest-server", log.LstdFlags)
-
+	/////// Initialize handlers here ///////
 	hh := handlers.NewHelloStruct(l)
 
+	//Serve Mux to replace the default ServeMux
 	sm := http.NewServeMux()
+
+	/////// Define route handlers for ServeMux here ///////
 	sm.Handle("/", hh)
 
+	// Configure the server {TODO: move these to an external configurable file/location}
 	server := &http.Server{
 		Addr:         ":9090",
 		Handler:      sm,
@@ -28,6 +33,7 @@ func main() {
 		WriteTimeout: 1 * time.Second,
 	}
 
+	// Asynchronously expose the server
 	go func() {
 		err := server.ListenAndServe()
 		if err != nil {
@@ -39,9 +45,12 @@ func main() {
 	signal.Notify(sigChan, os.Interrupt)
 	signal.Notify(sigChan, os.Kill)
 
+	//Block while not receiving forecfull shutdown
 	sig := <-sigChan
+
 	l.Println("Received terminate, graceful shutdown", sig)
 
+	//Define context to provide server on how to shutdown all running processes
 	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
 	server.Shutdown(tc)
 }
