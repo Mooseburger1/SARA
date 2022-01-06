fff130e2ce47969f7eec1f0b0a8ee832aaf56df3
Mooseburger1 <mooseburger1@msn.com>
2022-01-05 23:32:04 -0600

    started implementing grpc servers/services

---
+++ backend/grpc/server.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	protoPhotos "backend/grpc/proto/api/photos"
+	rpc "backend/grpc/rpc_servers"
+	"log"
+	"net"
+	"os"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+func main() {
+
+	logger := log.New(os.Stdout, "rpc-server", log.LstdFlags)
+
+	grpcServer := grpc.NewServer()
+	photoServer := rpc.NewGphotoServer(logger)
+
+	protoPhotos.RegisterGooglePhotoServiceServer(grpcServer, photoServer)
+
+	reflection.Register(grpcServer)
+	l, err := net.Listen("tcp", ":9092")
+	if err != nil {
+		logger.Fatal(err)
+		os.Exit(1)
+	}
+
+	grpcServer.Serve(l)
+}
