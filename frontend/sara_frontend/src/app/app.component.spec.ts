20d124e6e6db6843b97ea5c7ec9972f11fa693a5
Mooseburger1 <mooseburger1@msn.com>
2022-01-05 22:34:48 -0600

    removed nested .git folder

--- frontend/sara_frontend/src/app/app.component.spec.ts
+++ frontend/sara_frontend/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'sara'`, () => {
+  it(`should have as title 'sara_frontend'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('sara');
+    expect(app.title).toEqual('sara_frontend');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('sara app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('sara_frontend app is running!');
   });
 });
